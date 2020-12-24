215b3c2b2abf95976f231377761d32b7d9b773ab
josuemate2110 <josuemaquera@upeu.edu.pe>
2020-12-24 15:42:24 -0500

    DaoImp updated

--- src/main/java/pe/edu/upeu/lpexa1josuemaquera/daoImp/PedidosDaoImp.java
+++ src/main/java/pe/edu/upeu/lpexa1josuemaquera/daoImp/PedidosDaoImp.java
@@ -3,11 +3,13 @@ package pe.edu.upeu.lpexa1josuemaquera.daoImp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upeu.lpexa1josuemaquera.dao.PedidosDao;
 import pe.edu.upeu.lpexa1josuemaquera.entity.Pedidos;
+import pe.edu.upeu.lpexa1josuemaquera.entity.Platos;
 @Repository
 public class PedidosDaoImp implements PedidosDao{
 	@Autowired
@@ -15,13 +17,14 @@ public class PedidosDaoImp implements PedidosDao{
 	@Override
 	public List<Pedidos> readAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("select*from Pedidos", BeanPropertyRowMapper.newInstance(Pedidos.class));
 	}
 
 	@Override
 	public Pedidos readById(int Id) {
 		// TODO Auto-generated method stub
-		return null;
+		String SQL="select*from pedidos where idpedidos=?";
+		return jdbcTemplate.queryForObject(SQL, new Object[] {Id},new BeanPropertyRowMapper<Pedidos>(Pedidos.class));
 	}
 
 	@Override
